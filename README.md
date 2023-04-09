9abf01f3794197a82942937a05d8e6ad906a0df0
Omowunmijuin <balikiskamil@gmail.com>
2023-04-09 01:31:33 +0000

    Let's play in the snow

---
+++ README.md
@@ -0,0 +1,1 @@
+Practising python programing language
---
+++ snowFlakes.py
@@ -0,0 +1,12 @@
+# drawing a snowflake
+
+flake1 = "  *  "
+flake2 = " *** "
+flake3 = "*****"
+
+print(flake1)
+print(flake2)
+print(flake3)
+print(flake2)
+print(flake1)
+
