a1b7546335c31eb4968aea0dbd304fc26e58cb30
Omowunmijuin <balikiskamil@gmail.com>
2023-04-09 05:10:45 +0000

    Checking for account balance

---
+++ bank_acc_balance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" An ATM program that calculate and display a balance remaining after a withdrawal."""
+
+print("Enter your initial balance, followed by your withdrawal amount: ")
+initialBalance = int(input())
+withdrawal = int(input())
+
+initialBalance -= withdrawal
+print("Your remaining balance is: " + str(initialBalance)) 
