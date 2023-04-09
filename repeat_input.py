53ad521cd96175d499b1f4940e9f303716d65ad8
Omowunmijuin <balikiskamil@gmail.com>
2023-04-09 05:12:39 +0000

    Repeat input

--- repeat_input.py
+++ repeat_input.py
@@ -1,4 +1,6 @@
-# Write a program that repeat an input at the giving number of time
+#!/usr/bin/python3
+""" Write a program that repeat an input at the giving number of time """
+
 
 print("Enter something to be repeated: ")
 random_input = str(input())
