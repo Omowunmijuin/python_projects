a6ef4d0fe813a2408e844dc27ecab50681044515
Omowunmijuin <balikiskamil@gmail.com>
2023-04-09 01:48:22 +0000

    collecting inputs

---
+++ countryCard.py
@@ -0,0 +1,8 @@
+# making a program that takes and output Country and its Capital.
+
+print("Enter your Country and its Capital: ")
+country = input()
+capital = input()
+
+print("Your country is: " + country)
+print("And its capital is: " + capital)
