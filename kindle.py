99afc31470b71ee240429d7c8be51bdf8d8c308d
Omowunmijuin <balikiskamil@gmail.com>
2023-04-10 03:46:55 +0000

    giving out kindles!

---
+++ kindle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Write a program that take the user age and output Take your kindle! if age is under or equal to 19 """
+
+
+age = int(input())
+
+if age <= 19:
+    print("Take your kindle!")
+else:
+    print("Sorry no kindle for you!")
