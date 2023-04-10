f3c3808bb3c0465c5585a2657b095e3efa958a8d
Omowunmijuin <balikiskamil@gmail.com>
2023-04-10 04:00:30 +0000

    Clubing can be fun!

---
+++ club_access.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Write a program that checks and validates club entrance """
+
+
+print("Enter your age to check if you are eligible for clubing: ")
+age = int(input())
+
+if age >= 18:
+    print("Yay! let's go clubing")
+else:
+    print("sorry champ! give it a try when you hit 18")
