af0e6038ca9d44ab10526c7a01addb9fe1a3b295
Omowunmijuin <balikiskamil@gmail.com>
2023-04-10 03:38:36 +0000

    checking the temprature

---
+++ temprature_check.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" checking the temprature using if conditioning """
+
+
+print("Check the temprature: ")
+temp = int(input())
+
+if temp >= 100:
+    print("Boiling!")
+else:
+    print("chilled!")
