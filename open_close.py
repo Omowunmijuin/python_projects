a1df3e6a78a34d77b8fe3ea32b57f6bda9e2cbf5
Omowunmijuin <balikiskamil@gmail.com>
2023-04-11 12:28:52 +0000

    checking if the store is open today

---
+++ open_close.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a program that output whether a stor is open or closed, based on a giving day and hour of the week """
+
+
+day = int(input())
+hour = int(input())
+
+if day >= 0 or day <= 5:
+    if hour > 9 and hour <= 21:
+        print("We're open!")
+    else:
+        print("Sorry we're closed for today, pls check back tomorrow.")
+else:
+    print("Sorry the store is closed today!")
