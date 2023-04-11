d47e487c822772ade88519947a6b5cab678e261d
Omowunmijuin <balikiskamil@gmail.com>
2023-04-11 12:46:30 +0000

    checking if the store is open or closed

---
+++ open_close2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" using the boolean logic to check if a store is open or not on a specific day and hour """
+
+
+day = int(input())
+hour = int(input())
+
+if (day == 1 or day <= 5) and (hour > 9 and hour <= 21):
+    print("We're open!")
+else:
+    print("Sorry we're closed, check back tomorrow.")
