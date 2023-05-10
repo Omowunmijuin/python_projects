e9e142510d8c021a76067e8e3bfcba169878c85e
Omowunmijuin <balikiskamil@gmail.com>
2023-05-10 01:34:40 +0000

    Class

---
+++ class_student.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Program that uses the class instance to find the student attribute """
+
+
+class student:
+    def __init__(self, name)
+    self.name = name
+
+file = student("Tobi")
